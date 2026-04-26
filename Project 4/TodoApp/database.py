4a9f56bce75010cf48e035701bcbc21025e139aa
Wayne Yong <ymw0331@gmail.com>
2026-04-26 23:51:23 +0800

    complete project 3

---
+++ Project 4/TodoApp/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+
+# allow one thread to access the database at a time, prevent accidental data connection issues
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# db object is used to interact with the database, and is created for each request
+Base = declarative_base()
+
