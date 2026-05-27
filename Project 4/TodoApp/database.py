9f1690958b43b2e22b709eb9cb2238b84e153592
Wayne Yong <ymw0331@gmail.com>
2026-05-27 23:54:38 +0800

    project4: add user model

--- Project 4/TodoApp/database.py
+++ Project 4/TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 
 # allow one thread to access the database at a time, prevent accidental data connection issues
